61c50e361ddc606ffaed46c3eba88fb4a23b8cdf
unknown <A0123057@utpc46b.stf.nus.edu.sg>
2015-08-26 22:28:43 +0800

    While loop to find square

---
+++ while-loop.py
@@ -0,0 +1,11 @@
+# LOOP THROUGH X
+input = int(raw_input('Number to find square:'))
+x = input
+ans = 0
+iters = x
+while (iters != 0):
+    ans = ans + x
+    iters = iters - 1
+    print(ans)
+    print('')
+print(str(x)+"*"+str(x)+"="+str(ans))
