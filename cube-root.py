bfbcb11d0ccada4d65f8ab85baecf42435df0590
unknown <A0123057@utpc46b.stf.nus.edu.sg>
2015-08-26 23:17:27 +0800

    Cube root

---
+++ cube-root.py
@@ -0,0 +1,8 @@
+x = int(raw_input('ENTER A NUMBER TO FIND CUBE ROOT'))
+ans = 0
+while (ans**3 < x):
+    ans += 1
+if ans**3 != x:
+    print(str(x) + 'is not a perfect cube')
+else:
+    print('Cube root of ' +str(x) +'is '+ str(ans) )
